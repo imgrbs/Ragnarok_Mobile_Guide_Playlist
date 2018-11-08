539c1be94e5f0d5cda71b65399d6b83a44d9886e
Keerati <keerati.jear@gmail.com>
2018-11-08 18:37:02 +0700

    update hero

--- src/components/landing/index.js
+++ src/components/landing/index.js
@@ -4,6 +4,19 @@ import Container, { Col } from "../base/layout";
 import Card from "../base/card";
 import Header from "../base/header";
 
+const Hero = () => (
+  <section class="jumbotron text-center">
+    <div class="container">
+      <h1 class="jumbotron-heading">Album example</h1>
+      <p class="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don't simply skip over it entirely.</p>
+      <p>
+        <a href="#" class="btn btn-primary m-2">Main call to action</a>
+        <a href="#" class="btn btn-secondary m-2">Secondary action</a>
+      </p>
+    </div>
+  </section>
+)
+
 export default class LandingIndex extends Component {
   state = {
     videos: [1, 2, 3, 4, 5]
@@ -11,6 +24,9 @@ export default class LandingIndex extends Component {
   render() {
     return (
       <Container>
+        <Col>
+          <Hero />
+        </Col>
         <Col>
           <Header>Playlists</Header>
         </Col>
