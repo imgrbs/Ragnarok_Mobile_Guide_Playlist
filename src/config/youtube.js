7cf50eb0a45dfdde3f5d6fd0386645059ebe334f
Keerati <keerati.jear@gmail.com>
2018-11-14 00:32:58 +0700

    update maxresult

--- src/config/youtube.js
+++ src/config/youtube.js
@@ -1,4 +1,4 @@
 
 export const API_URL = "https://www.googleapis.com/youtube/v3";
 export const KEY = "AIzaSyBXfuNn2vUsaXQS5eII91eIHLmuCuWfurM";
-export const PLAYLIST_ENDPOINT = `playlistItems?part=snippet%2Cid%2CcontentDetails%2Cstatus&maxResults=50&key=${KEY}`;
+export const PLAYLIST_ENDPOINT = `playlistItems?part=snippet%2Cid%2CcontentDetails%2Cstatus&maxResults=12&key=${KEY}`;
